e995caef0f39a9641788b483ecc2c3cd774319c2
owenliang <120848369@qq.com>
2017-08-07 14:43:19 +0800

    course1 update

---
+++ course1-how-to-export-a-module/myext.c
@@ -0,0 +1,50 @@
+#include "myext.h"
+
+int extension_startup(int type, int module_number) {
+    TRACE("extension_startup");
+    return SUCCESS;
+}
+
+int extension_shutdown(int type, int module_number) {
+    TRACE("extension_shutdown");
+    return SUCCESS;
+}
+
+int extension_before_request() {
+    TRACE("extension_before_request");
+    return SUCCESS;
+}
+
+int extension_after_request() {
+    TRACE("extension_after_request");
+    return SUCCESS;
+}
+
+void extension_info(zend_module_entry *zend_module) {
+    php_info_print_table_start();
+    php_info_print_table_header(2, "myext support", "enabled");
+    php_info_print_table_row(2, "author", "owenliang");
+    php_info_print_table_row(2, "course name", "course1-how-to-export-a-module");
+    php_info_print_table_end();
+}
+
+zend_module_entry module = {
+    STANDARD_MODULE_HEADER_EX,  // size,zend_api,zend_debug,zts
+    NULL,   // ini_entry
+    NULL,   // deps
+    "myext",    //name
+    NULL,   // functions
+    extension_startup,  // module_startup_func
+    extension_shutdown, // module_shutdown_func
+    extension_before_request,   // request_startup_func
+    extension_after_request,    // request_shutdown_func
+    extension_info, // info_func
+    "1.0",  // version
+    // globals_size,globals_ptr,globals_ctor,globals_dtor,post_deactivate_func,module_started,type,
+    // handle,module_number,build_id
+    STANDARD_MODULE_PROPERTIES,
+};
+
+ZEND_DLEXPORT zend_module_entry *get_module() {
+    return &module;
+}
