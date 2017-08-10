7186169ebac00a8e2204042f9714dd6662c4c908
owenliang <120848369@qq.com>
2017-08-10 15:31:01 +0800

    update course7

---
+++ course7-how-to-create-object/test.php
@@ -0,0 +1,22 @@
+<?php
+
+// parent
+$obj = new MyExt();
+
+echo $obj->strtolower("HELLO") . $obj->strtoupper("php") . $obj->strtolower(2017) . PHP_EOL;
+echo $obj->version() . PHP_EOL;
+echo MyExt::$author . PHP_EOL;
+echo MyExt::BUILD_DATE . PHP_EOL;
+
+echo "====================" . PHP_EOL;
+
+// child
+$obj = new MyExt_Child();
+
+echo $obj->strtolower("HELLO") . $obj->strtoupper("php") . $obj->strtolower(2017) . PHP_EOL;
+echo $obj->version() . PHP_EOL;
+echo MyExt_Child::$author . PHP_EOL;
+echo MyExt_Child::BUILD_DATE . PHP_EOL;
+
+// global function
+test_object();
