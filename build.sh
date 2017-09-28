eb45176d276ae498bcb426c0945ecb47f39b40ff
liangdong <liangdong@smzdm.com>
2017-09-28 14:21:41 +0800

    ci

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
+# 只编译不安装, 若需安装某个course需自己执行make install
+
 set -e
 
 for c in course*;do
-	cd $c 
-	make
-	sudo make install
-	make test
-	cd -
+        cd $c && make && make && cd -
 done
 
+sudo echo "extension=myext.so" >> `php --ini|grep Loaded| awk '{print $NF}'`
